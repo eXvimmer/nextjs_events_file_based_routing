a2c6b01b29d38a29ec8dd422f92b4fe077c95767
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-06-15 01:07:22 +0430

    Complete the all events page.

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,11 @@
+import { getAllEvents } from "../../dummy_data";
+import EventList from "../../components/Events/EventList";
+
 function AllEventsPage() {
+  const events = getAllEvents();
   return (
     <div>
-      <h1>All Events</h1>
+      <EventList items={events} />
     </div>
   );
 }
